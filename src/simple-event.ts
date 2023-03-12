d22e793a5fa086bc672636320319763d9dea825c
farrokh <farrokh.charoghchi@gmail.com>
2023-03-12 14:37:58 +0330

    simple event class created

--- src/simple-event.ts
+++ src/simple-event.ts
@@ -1,6 +1,14 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+import { EventEmitter } from "events";
 
+class SimpleEvent{
+    private event = new EventEmitter();
+    private eventName="defaultEvent"
+    subscribe(listener: (...args: any[]) => void){
+        this.event.on(this.eventName, listener);
+    }
+    emit(...args: any[]){
+        this.event.emit(this.eventName, args);
+    }
 }
+
+export default SimpleEvent
