90aa4b9273952b69b0bc0f5c0e7746382ae80388
farrokh <farrokh.charoghchi@gmail.com>
2023-03-12 19:39:55 +0330

    fix: import fixed
    import statement changed to default import

--- src/simple-event.ts
+++ src/simple-event.ts
@@ -1,14 +1,14 @@
-import { EventEmitter } from "events";
+import EventEmitter from 'events'
 
-class SimpleEvent{
-    private event = new EventEmitter();
-    private eventName="defaultEvent"
-    subscribe(listener: (...args: any[]) => void){
-        this.event.on(this.eventName, listener);
-    }
-    emit(...args: any[]){
-        this.event.emit(this.eventName, args);
-    }
+class SimpleEvent {
+  private event = new EventEmitter()
+  private eventName = 'defaultEvent'
+  subscribe(listener: (...args: any[]) => void) {
+    this.event.on(this.eventName, listener)
+  }
+  emit(...args: any[]) {
+    this.event.emit(this.eventName, args)
+  }
 }
 
 export default SimpleEvent
